2afc49a7ba13e256e01a0a3c2a72c3ee2113862a
vihahuynh <huynhviha1703@gmail.com>
2021-11-03 22:56:54 +0700

    Complete 10.12

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { NativeRouter } from "react-router-native";
+import Constants from "expo-constants";
 import Main from "./src/components/Main";
 
 import createApolloClient from "./src/utils/apolloClient";
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest);
   return (
     <>
       <NativeRouter>
